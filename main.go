f1f9486c2e5731943e2b9076f9e038b341e22653
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-04-24 16:34:21 +0200

    🚧 work in progress

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cart/w4"
+	"math/rand"
 	"strconv"
 )
 
@@ -9,6 +10,8 @@ const (
 	WIDTH  = 160
 	HEIGHT = 160
 )
+
+type Color = uint8
 
 func print(xs ...interface{}) {
 	s := ""
@@ -40,6 +43,7 @@ func print(xs ...interface{}) {
 	w4.Trace(s[:len(s)-1])
 }
 
+// TODO: add pixel iterator and width & height functions
 type Image interface {
 	getPixel(x, y int) (color uint8)
 	setPixel(x, y int, color uint8)
@@ -90,10 +94,6 @@ var screen = (*Screen)(w4.FRAMEBUFFER)
 type Image3x3 [3][3]uint8 // Not a multiple of four pixels, can't use
 // and don't need to) use the compact layout used for the screen image.
 
-// TODO. Redefine Mask as 3x3 buffer and make corresponding method getPixel,
-// setPixels. Define an interface(?). And make some getSubScreen, setSubScreen
-// stuff?
-
 func (image *Image3x3) getPixel(x, y int) (color uint8) {
 	x = ((x % 3) + 3) % 3
 	y = ((y % 3) + 3) % 3
@@ -125,22 +125,107 @@ func (screen *Screen) setImage3x3(x, y int, image3x3 Image3x3) {
 	}
 }
 
+type Rule func(image3x3 Image3x3) (color Color, match bool)
+
+func (image3x3 *Image3x3) colorDistribution() (count map[Color]int) {
+	count = make(map[Color]int)
+	for _, color := range []Color{0, 1, 2, 3} {
+		count[color] = 0
+	}
+	for y := 0; y < 3; y++ {
+		for x := 0; x < 3; x++ {
+			color := image3x3.getPixel(x, y)
+			count[color] += 1
+		}
+	}
+	return
+}
+
+func grassSpreads(image3x3 Image3x3) (color Color, match bool) {
+	if image3x3.getPixel(1, 1) == 0 {
+		count := image3x3.colorDistribution()
+		if count[1] >= 1 {
+			match = true
+			color = 1
+		}
+	}
+	return
+}
+
+func rabbitsEatGrass(image3x3 Image3x3) (color Color, match bool) {
+	if image3x3.getPixel(1, 1) == 1 {
+		count := image3x3.colorDistribution()
+		if count[2] >= 1 {
+			match = true
+			color = 2
+		}
+	}
+	return
+}
+
+func tooManyRabbits(image3x3 Image3x3) (color Color, match bool) {
+	if image3x3.getPixel(1, 1) == 3 {
+		count := image3x3.colorDistribution()
+		if count[2] >= 8 {
+			match = true
+			color = 0
+		}
+	}
+	return
+}
+
+func tooManywolves(image3x3 Image3x3) (color Color, match bool) {
+	if image3x3.getPixel(1, 1) == 3 {
+		count := image3x3.colorDistribution()
+		if count[3] >= 3 {
+			match = true
+			color = 0
+		}
+	}
+	return
+}
+
+func wolvesKillRabbits(image3x3 Image3x3) (color Color, match bool) {
+	if image3x3.getPixel(1, 1) == 2 {
+		count := image3x3.colorDistribution()
+		if count[3] >= 1 {
+			match = true
+			color = 3
+		}
+	}
+	return
+}
+
+var rules = []Rule{
+	grassSpreads,
+	rabbitsEatGrass,
+	tooManyRabbits,
+	tooManywolves,
+	wolvesKillRabbits,
+}
+
 func next() {
 	for y := 0; y < HEIGHT; y++ {
 		for x := 0; x < WIDTH; x++ {
 			image3x3 := buffer.getImage3x3(x-1, y-1)
-			color := image3x3.getPixel(1, 1)
-			if color > 0 {
-				for j := 0; j < 3; j++ {
-					for i := 0; i < 3; i++ {
-						image3x3.setPixel(i, j, color)
-					}
+			for _, rule := range rules {
+				color, match := rule(image3x3)
+				if match {
+					screen.setPixel(x, y, color)
+					break
 				}
-				screen.setImage3x3(x-1, y-1, image3x3)
 			}
 		}
 	}
 	*buffer = *screen
+}
+
+func initScreen() {
+	for j := 0; j < HEIGHT; j++ {
+		for i := 0; i < WIDTH; i++ {
+			screen.setPixel(i, j, uint8(rand.Intn(4)))
+		}
+	}
 }
 
 //go:export start
@@ -151,11 +236,25 @@ func start() {
 	w4.PALETTE[3] = 0x495057
 }
 
+var first = true
+
 //go:export update
 func update() {
-	if *w4.MOUSE_BUTTONS&0b1 != 0 {
-		x, y := int(*w4.MOUSE_X), int(*w4.MOUSE_Y)
-		buffer.setPixel(x, y, 3)
+
+	if first {
+		initScreen()
+		first = false
+	} else {
+		if *w4.MOUSE_BUTTONS&0b1 != 0 {
+			x, y := int(*w4.MOUSE_X), int(*w4.MOUSE_Y)
+			for j := 0; j < 21; j++ {
+				for i := 0; i < 21; i++ {
+					buffer.setPixel(x-10+i, y-10+j, 2)
+				}
+			}
+		}
+		*screen = *buffer
 	}
+
 	next()
 }
